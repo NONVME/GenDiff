6134f30fda2bd9651107efa7db808d07e5f8dba2
NONVME <efimmm.ev@gmail.com>
2020-10-20 03:55:41 +0000

    feat: add arguments and options in help prompt

--- gendiff/args.py
+++ gendiff/args.py
@@ -8,5 +8,6 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', help='\n set format of output')
 
 args = parser.parse_args()
