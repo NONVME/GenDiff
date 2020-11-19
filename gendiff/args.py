a40a6f81e2259da509076888c1467def3393a442
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2020-11-19 10:51:57 +0000

    refactor: explicit argparse use

--- gendiff/args.py
+++ gendiff/args.py
@@ -5,18 +5,19 @@ from gendiff.formatters.stylish import PRETTY, PLAIN, JSON
 from gendiff import generate_diff
 
 
-parser = argparse.ArgumentParser(
-    description='Generate diff',
-    prog='gendiff',
-)
-parser.add_argument('first_file', type=str)
-parser.add_argument('second_file', type=str)
-parser.add_argument('-f',
-                    '--format',
-                    help='\n set format of output',
-                    choices=[PRETTY, PLAIN, JSON],
-                    default=PRETTY,
-                    type=str,
-                    )
+def get_args_parser():
+    parser = argparse.ArgumentParser(
+        description='Generate diff',
+        prog='gendiff',
+    )
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f',
+                        '--format',
+                        help='\n set format of output',
+                        choices=[PRETTY, PLAIN, JSON],
+                        default=PRETTY,
+                        type=str,
+                        )
 
-args = parser.parse_args()
+    return parser
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,12 @@
 """The main script."""
 
 from gendiff import generate_diff
-from gendiff.args import args
+from gendiff.args import get_args_parser
 
 
 def main():
     """Select format selection."""
+    args = get_args_parser().parse_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
 
